119dd3fb9c674777d02b6ac2f25d338ca0dc111f
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-02-10 03:10:56 +0200

    Generation of automation graphs

--- smauto_cli/cli.py
+++ smauto_cli/cli.py
@@ -2,6 +2,7 @@ import sys
 import click
 
 from smauto.interpreter import interpret_model_from_path
+from smauto.generator import generate_automation_graph_from_file
 
 
 @click.group()
@@ -15,6 +16,13 @@ def cli(ctx):
 @click.argument('model_path')
 def interpret(ctx, model_path):
     interpret_model_from_path(model_path)
+
+
+@cli.command(help='Automation Graph generator')
+@click.pass_context
+@click.argument('model_path')
+def gen_automation_graph(ctx, model_path):
+    generate_automation_graph_from_file(model_path)
 
 
 def main():
